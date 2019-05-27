8cde762b679cbcc37e8d4ce6dab0254bfbf67404
Marshal <iamkaranvir93@gmail.com>
2019-05-27 15:34:05 -0400

    Added react-router-dom

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -1,10 +1,17 @@
 import React, {Component} from 'react'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 
 class TodoApp extends Component{
     render(){
         return(
             <div className="TodoApp">
-                <LoginComponent />
+                <Router>
+                    <>
+                    <Route path ="/" exact component = {LoginComponent} />
+                    <Route path ="/login" component = {LoginComponent} />
+                    <Route path ="/welcome" component ={WelcomeComponent} />
+                    </>
+                </Router>
             </div>
         )
     }
@@ -93,12 +100,15 @@ function CheckCredentials(props){
     else return null
 }
 
-function InvalidCredentials(props){
-    if(props.hasLoginFailed == true){
-        return <div>Invalid Credentials</div>
+class WelcomeComponent extends Component{
+    render(){
+        return(
+            <div>
+                Welcome Marshal !!
+            </div>
+        )
     }
-    else
-        return null
 }
+
 
 export default TodoApp
