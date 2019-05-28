2717169d14cba2d34b2ed628ca05c59b3cc8c089
Marshal <iamkaranvir93@gmail.com>
2019-05-28 00:55:22 -0400

    Created menu with navigation links using bootstrap

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -123,7 +123,19 @@ class WelcomeComponent extends Component{
 class HeaderComponent extends Component{
     render(){
         return(
-           <div>Header <hr /></div> 
+           <header>
+               <nav className="navbar navbar-expand-md navbar-dark bg-dark" >
+                   <div><a className="navbar-brand" href="https://www.linkedin.com/in/karanvir-singh-sidhu/">Karanvir</a></div>
+                   <ul className="navbar-nav">
+                       <li ><Link className="nav-link" to="/welcome/Marshal">Home</Link></li>
+                       <li ><Link className="nav-link" to="/todos">Todos</Link></li>
+                   </ul>
+                   <ul className="navbar-nav navbar-collapse justify-content-end">
+                       <li ><Link className="nav-link" to="/login">Login</Link></li>
+                       <li ><Link className="nav-link" to="/login">Logout</Link></li>
+                   </ul>
+               </nav>
+           </header>
         )
     }
 }
